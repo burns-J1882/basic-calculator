2e3d48c6f5389865c51d6dcdc89d8352476facbc
James Burns <burns.j1882@gmail.com>
2022-09-02 13:31:09 -0400

    Create basic operator functions and a calling function

---
+++ app.js
@@ -0,0 +1,21 @@
+//basic operator functions
+function add (a, b){
+    return a + b;
+};
+
+function subtract (a, b){
+    return a - b;
+};
+
+function multiply (a, b){
+    return a * b;
+};
+
+function divide (a, b){
+    return a / b;
+}
+
+//function that calls operator
+function operate (operator, a, b){
+    return operator(a, b);
+}
